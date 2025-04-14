39f1e83dde7a4937320261df0b043d3d2148a7c6
anton <avainfoconseils@gmail.com>
2025-04-14 15:34:35 +0200

    Initialize Hand gesture detector

---
+++ dev/hand-gesture-detector/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <mqtt/async_client.h>
+
+int main() {
+    std::cout << "Hello from Ava Gesture Detector!" << std::endl;
+
+    return 0;
+}
